c7aa80a04d55f4b26eddea0e3dadcc51d0084ead
Umar Farooq <114721771+Umarfarooq2545@users.noreply.github.com>
2025-07-27 23:57:42 +0500

    new

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -3,6 +3,10 @@
 describe('URL Test', () => {
   it('should login with valid credentials', () => {
     cy.visit('https://stg.platform.creatingly.com/apps/');
-    //cy.contains("Get started");
+     cy.get('div[role="button"][name="Draw Artboard"]').click();
+
+  cy.get('div[role="button"][name="Desktop Template1"]')
+    .should('be.visible').click();
+
   });
 })
